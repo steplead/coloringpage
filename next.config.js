692447e91788a4b9e52a5e131cce34a4f83cdfbf
steplead <your.email@example.com>
2025-04-03 10:03:21 +0800

    将Next.js图片配置改为unoptimized:true, 解决阿里云OSS图片处理问题

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['sc-maas.oss-cn-shanghai.aliyuncs.com'],
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
